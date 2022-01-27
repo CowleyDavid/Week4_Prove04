0e6d2e651faf00f5273dc63bb16a1ac66b6d9887
Oleg Lasukov <lasukov2016@mail.ru>
2022-01-27 09:59:58 +0500

    Forth commit

--- src/Prove04_Cowley/Main.java
+++ src/Prove04_Cowley/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// First Print statement
         System.out.println("Hello my name is David");
-        System.out.println("Hello David!");
+        System.out.println("Hello Oleg!");
         System.out.println("Hello Claudia!");
+
+        System.out.println("Hello again");
 
 
     }
