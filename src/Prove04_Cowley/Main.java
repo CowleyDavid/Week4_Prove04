70bea031fbb4d90e359bf7722c4929b1ea1ceea4
CowleyDavid <cow17002@byui.edu>
2022-01-26 21:57:03 -0700

    fith message

--- src/Prove04_Cowley/Main.java
+++ src/Prove04_Cowley/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// First Print statement
         System.out.println("Hello my name is David");
+        System.out.println("David");
     }
 }
